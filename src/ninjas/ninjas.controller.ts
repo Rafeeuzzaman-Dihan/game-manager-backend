aaeb673c227067ce7caad3147147b094ff9bb989
Dihan <rafeeuzzamandihan@gmail.com>
2025-08-11 16:25:47 +0600

    Add UpdateNinjaDto and update @Put method in NinjasController to use typed body parsing

--- src/ninjas/ninjas.controller.ts
+++ src/ninjas/ninjas.controller.ts
@@ -9,6 +9,7 @@ import {
   Body,
 } from '@nestjs/common';
 import { CreateNinjaDto } from './dto/create-ninja.dto';
+import { UpdateNinjaDto } from './dto/update-ninja.dto';
 
 @Controller('ninjas')
 export class NinjasController {
@@ -43,9 +44,10 @@ export class NinjasController {
 
   // Put Method
   @Put(':id')
-  updateNinja(@Param('id') id: string) {
+  updateNinja(@Param('id') id: string, @Body() updateNinjaDto: UpdateNinjaDto) {
     return {
       id,
+      name: updateNinjaDto,
     };
   }
 
