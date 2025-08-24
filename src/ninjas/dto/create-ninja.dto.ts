92e3c501b76cda36deb402b2f5f6b05aded37cf8
Dihan <rafeeuzzamandihan@gmail.com>
2025-08-24 12:12:19 +0600

    Add initial `package-lock.json` for dependency management

--- src/ninjas/dto/create-ninja.dto.ts
+++ src/ninjas/dto/create-ninja.dto.ts
@@ -1,7 +1,8 @@
-import { MinLength } from 'class-validator';
+import { IsEnum, MinLength } from 'class-validator';
 export class CreateNinjaDto {
   @MinLength(3)
   name: string;
 
+  @IsEnum(['stealth', 'speed', 'strength'], {message: 'Type must be one of the following: stealth, speed, strength'})
   type: 'stealth' | 'speed' | 'strength';
 }
